bfb90cef0be2acae048f3bca440010f431a190cc
Andre Wachsmuth <awa@xima.de>
2025-03-30 08:30:40 +0200

    Add demo test for moxy bug regarding namespace of DOM attributes

--- src/test/java/bugdemo/Testcase.java
+++ src/test/java/bugdemo/Testcase.java
@@ -1,108 +1,58 @@
 package bugdemo;
 
 import org.eclipse.persistence.jaxb.JAXBContext;
+import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Element;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLOutputFactory;
-import java.io.IOException;
-import java.io.StringReader;
 import java.io.StringWriter;
 
 /**
- * This appears to be a bug in EclipseLink MOXy whereby if an XmlAnyElement has no namespace then a duplicate
- * default namepace tag is defined when emitting to an XMLStreamWriter
+ * This appears to be a bug in EclipseLink MOXy. When marshalling an org.w3c.dom.Element, it includes the namespace
+ * from the element, but not from its org.w3c.dom.Attr. It's also missing the prefix, putting the attribute in the
+ * default namespace (which is wrong).
  */
 public class Testcase
 {
-	private static final String INNER_DOC = "<x xmlns=\"\"><foo></foo></x>";
-
-	@Test //expected=org.xml.sax.SAXParseException.class
+	@Test
 	public void demoBug() throws Exception
 	{
 		MyXmlAnyElementType el = new MyXmlAnyElementType();
-		el.someXmlBlock = parse(INNER_DOC).getDocumentElement();
+		Document doc = newDocument();
+		Element x = doc.createElementNS("http://example.com/element", "x");
+		x.setAttributeNS("http://example.com/attribute", "attr", "value");
+		el.someXmlBlock = x;
 
 		StringWriter sw = new StringWriter();
-		final Marshaller marshaller = getMarshaller(MyXmlAnyElementType.class);
-		marshaller.marshal(el, XMLOutputFactory.newInstance().createXMLStreamWriter(sw));
+		Marshaller marshaller = getMarshaller(MyXmlAnyElementType.class);
+		marshaller.marshal(el, sw);
 
-		System.out.println("Should be invalid XML: " + sw);
+		System.out.println("Includes namespace from element, but not attribute: " + sw);
 
-		// N.B. This blows up if invalid XML was output
-		parse(sw.toString());
+		Assert.assertTrue(sw.toString().contains("http://example.com/element"));
+		Assert.assertTrue(sw.toString().contains("http://example.com/attribute"));
 	}
 
-
-	@Test
-	public void testWorkaround() throws Exception
-	{
-		MyXmlAnyElementType el = new MyXmlAnyElementType();
-		el.someXmlBlock = parse(INNER_DOC).getDocumentElement();
-
-		StringWriter sw = new StringWriter();
-		final Marshaller marshaller = getMarshaller(MyXmlAnyElementType.class);
-		marshaller.marshal(el, new DuplicateNSFilteringXMLStreamWriter(XMLOutputFactory.newInstance().createXMLStreamWriter(sw)));
-
-		// N.B. This blows up if invalid XML was output
-		parse(sw.toString());
-	}
-
-
-	@Test
-	public void demoBugOnlyImpactsDefaultNamespace() throws Exception
-	{
-		// Test being in a non-default namespace
-		MyXmlAnyElementType el = new MyXmlAnyElementType();
-		el.someXmlBlock = parse("<x xmlns=\"urn:foo\" />").getDocumentElement();
-
-		StringWriter sw = new StringWriter();
-		final Marshaller marshaller = getMarshaller(MyXmlAnyElementType.class);
-		marshaller.marshal(el, XMLOutputFactory.newInstance().createXMLStreamWriter(sw));
-
-		// N.B. This blows up if invalid XML was output
-		parse(sw.toString());
-	}
-
-
-	@Test
-	public void demoBugOnlyImpactsDefaultNamespaceIfDeclared() throws Exception
-	{
-		// Test implicitly being in the default namespace
-		MyXmlAnyElementType el = new MyXmlAnyElementType();
-		el.someXmlBlock = parse("<x />").getDocumentElement();
-
-		StringWriter sw = new StringWriter();
-		final Marshaller marshaller = getMarshaller(MyXmlAnyElementType.class);
-
-		marshaller.marshal(el, XMLOutputFactory.newInstance().createXMLStreamWriter(sw));
-
-		// N.B. This blows up if invalid XML was output
-		parse(sw.toString());
-	}
-
-
-	private Document parse(final String xml) throws ParserConfigurationException, IOException, SAXException
+	private Document newDocument() throws ParserConfigurationException
 	{
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
 		factory.setNamespaceAware(true);
 		factory.setExpandEntityReferences(false);
 		factory.setXIncludeAware(false);
-		return factory.newDocumentBuilder().parse(new InputSource(new StringReader(xml)));
+		return factory.newDocumentBuilder().newDocument();
 	}
 
 
 	private Marshaller getMarshaller(Class<?> clazz) throws JAXBException
 	{
-		final javax.xml.bind.JAXBContext ctx = JAXBContext.newInstance(clazz);
-
-		return ctx.createMarshaller();
+		javax.xml.bind.JAXBContext ctx = JAXBContext.newInstance(clazz);
+		Marshaller marshaller = ctx.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		return marshaller;
 	}
 }
